d61f63b684cd60c4b8c3ca9905f6935a0da90205
wu_ling <17612774808@163.com>
2020-04-19 22:51:36 +0800

    新增博客系统UI界面（首页、归档页）

---
+++ blog/js/common.js
@@ -0,0 +1,18 @@
+$(function() {
+
+	navTopHideAndScrollShow();
+})
+
+function navTopHideAndScrollShow() {
+	$(window).scroll(function() {
+		let scrollPos = $(window).scrollTop();
+		if (scrollPos > 0) {
+			$(".nav-top").removeClass("none");
+			$(".nav-top").removeClass("nav-top-hide");
+			$(".nav-top").addClass("nav-top-show");
+		} else {
+			$(".nav-top").removeClass("nav-top-show");
+			$(".nav-top").addClass("nav-top-hide");
+		}
+	});
+}
