87d79fdc6d0c4243064b95a017686bff35f851f5
wu_ling <hxxian808@gmail.com>
2020-04-24 23:55:48 +0800

    关于我页面新增回复按钮动画显示隐藏，新增底部弹出回复框

--- blog/js/comment.js
+++ blog/js/comment.js
@@ -4,15 +4,37 @@ $(function() {
 		el: '#bloger',
 		data: {
 			replyShow: false,
+			readyReply: false,
+			replyBtnText: "回复"
 		},
 		methods: {
 			itemMouseEnter: function(idx) {
 				this.replyShow = true;
-				console.log("=======")
 			},
 			itemMouseLeave: function(idx) {
 				this.replyShow = false;
-				console.log("-------")
+			},
+			showReplyBox: function() {
+				if (this.readyReply) {
+					this.hideReplyBox();
+					return;
+				}
+				this.readyReply = true;
+				this.replyBtnText = "取消回复";
+				$(".bottom-send-box").removeClass("top-leave");
+				$(".bottom-send-box").addClass("bottom-enter");
+				
+				$(".item-reply-btn").removeClass("reply-btn");
+				$(".item-reply-btn").addClass("cancel-reply");
+			},
+			hideReplyBox: function() {
+				this.readyReply = false;
+				this.replyBtnText = "回复";
+				$(".bottom-send-box").removeClass("bottom-enter");
+				$(".bottom-send-box").addClass("top-leave");
+				
+				$(".item-reply-btn").removeClass("cancel-reply");
+				$(".item-reply-btn").addClass("reply-btn");
 			}
 		}
 	})
