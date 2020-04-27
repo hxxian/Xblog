cad42d794024066d21e53f48647633315fb1563e
wu_ling <hxxian808@gmail.com>
2020-04-27 08:33:08 +0800

    文章列表页新增文章类别栏目

--- blog/js/article-list.js
+++ blog/js/article-list.js
@@ -1,12 +1,14 @@
 $(function() {
 	
-	var archive = new Vue({
+	var article = new Vue({
 		el: '#bloger',
 		data: {
-			
+			showType: false
 		},
 		methods: {
-			
+			showArticleType: function() {
+				this.showType = !this.showType;
+			}
 		}
 	})
 	
