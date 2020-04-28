834d73ec1deff69f0dc68f96b32903623c9ad0ea
wu_ling <hxxian808@gmail.com>
2020-04-28 08:29:41 +0800

    1.将每个页面的主标题文字加阴影；2.文章页新增类别框收起动画、页码

--- blog/js/article-list.js
+++ blog/js/article-list.js
@@ -3,11 +3,17 @@ $(function() {
 	var article = new Vue({
 		el: '#bloger',
 		data: {
-			showType: false
+			showType: false,
+			closeTypeTag: 0
 		},
 		methods: {
 			showArticleType: function() {
 				this.showType = !this.showType;
+				if (!this.showType) {
+					this.closeTypeTag = 1;
+				} else {
+					this.closeTypeTag = 2;
+				}
 			}
 		}
 	})
