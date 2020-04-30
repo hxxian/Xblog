5ec1abdd4b2bcaabe412a96b3150d281a0a3b2dc
wu_ling <hxxian808@gmail.com>
2020-04-30 08:34:51 +0800

    新增文章详情页

---
+++ blog/js/article.js
@@ -0,0 +1,21 @@
+$(function() {
+	
+	var article = new Vue({
+		el: '#bloger',
+		data: {
+			showType: false,
+			closeTypeTag: 0
+		},
+		methods: {
+			showArticleType: function() {
+				this.showType = !this.showType;
+				if (!this.showType) {
+					this.closeTypeTag = 1;
+				} else {
+					this.closeTypeTag = 2;
+				}
+			}
+		}
+	})
+	
+})
