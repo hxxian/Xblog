8961d92a9127df87f73c9528b6aad88881716d95
wu_ling <hxxian808@gmail.com>
2020-05-01 19:00:59 +0800

    重构了关于我页面UI

--- blog/js/archive.js
+++ blog/js/archive.js
@@ -1,13 +1,27 @@
 $(function() {
-	
+
 	var archive = new Vue({
 		el: '#bloger',
 		data: {
-			
+			weeks: []
 		},
 		methods: {
-			
+
 		}
 	})
-	
+
+	archive.weeks = getWeeksOfYear();
+
 })
+
+function getWeeksOfYear() {
+	let obj = {
+		'date': '2020-05-01',
+		'content': 'test op'
+	}
+	let weeks = [];
+	for (var i = 0; i < 52; i++) {
+		weeks[i] = obj;
+	}
+	return weeks;
+}
