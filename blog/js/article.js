8199b356cd87d9d077c20ffbbcd6d68d1349759a
wu_ling <hxxian808@gmail.com>
2020-05-01 10:43:43 +0800

    完成文章详情页UI

--- blog/js/article.js
+++ blog/js/article.js
@@ -4,7 +4,10 @@ $(function() {
 		el: '#bloger',
 		data: {
 			showType: false,
-			closeTypeTag: 0
+			closeTypeTag: 0,
+			replyShow: false,
+			readyReply: false,
+			replyBtnText: "回复"
 		},
 		methods: {
 			showArticleType: function() {
@@ -14,6 +17,34 @@ $(function() {
 				} else {
 					this.closeTypeTag = 2;
 				}
+			},
+			itemMouseEnter: function(idx) {
+				this.replyShow = true;
+			},
+			itemMouseLeave: function(idx) {
+				this.replyShow = false;
+			},
+			showReplyBox: function() {
+				if (this.readyReply) {
+					this.hideReplyBox();
+					return;
+				}
+				this.readyReply = true;
+				this.replyBtnText = "取消回复";
+				$(".bottom-send-box").removeClass("top-leave");
+				$(".bottom-send-box").addClass("bottom-enter");
+				
+				$(".item-reply-btn").removeClass("reply-btn");
+				$(".item-reply-btn").addClass("cancel-reply");
+			},
+			hideReplyBox: function() {
+				this.readyReply = false;
+				this.replyBtnText = "回复";
+				$(".bottom-send-box").removeClass("bottom-enter");
+				$(".bottom-send-box").addClass("top-leave");
+				
+				$(".item-reply-btn").removeClass("cancel-reply");
+				$(".item-reply-btn").addClass("reply-btn");
 			}
 		}
 	})
