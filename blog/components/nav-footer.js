e93fd96ad748573bbd51d9570cef75be8aee518f
wu_ling <hxxian808@gmail.com>
2020-05-22 09:49:26 +0800

    新增底部页脚社交平台逻辑处理

--- blog/components/nav-footer.js
+++ blog/components/nav-footer.js
@@ -1,4 +1,7 @@
 $(function() {
+	// var strPath = window.document.location.pathname;
+	// var postPath = strPath.substring(0, strPath.substr(1).indexOf('/') + 1);
+	// var url = "http://" + window.location.host + postPath + "/";
 	Vue.component('nav-footer', {
 		data: function() {
 			return {}
@@ -7,13 +10,36 @@ $(function() {
 			gotoICPSystem: function() {
 				// window.location.href = "http://www.beian.miit.gov.cn/publish/query/indexFirst.action";
 				window.location.href = "http://www.beian.miit.gov.cn";
+			},
+			gotoContact: function(type) {
+				// 1 - github, 2 - csdn, 3 - wehcat, 4 - QQ, 5 - email
+				if (type == 1) {
+					window.open("https://github.com/hxxian");
+				} else if (type == 2) {
+					window.open("https://me.csdn.net/h_xiao_x");
+				} else if (type == 3) {
+					layer.photos({
+						photos: wxqrcode,
+						anim: 0 //0-6的选择，指定弹出图片动画类型，默认随机
+					});
+				} else if (type == 4) {
+					layer.photos({
+						photos: qqQrcode,
+						anim: 0 //0-6的选择，指定弹出图片动画类型，默认随机
+					});
+				}
+				else if (type == 5) {
+					layer.tips('363756222@qq.com', '#me-email', {
+						tips: [1, '#0FA6D8']
+					});
+				}
 			}
 		},
 		template: footer
 	})
 })
 
-var footer = 
+var footer =
 	'<div class="nav-footer"> ' +
 	'	<div class="link-container"> ' +
 	'		<div class="qrcode"> ' +
@@ -21,19 +47,19 @@ var footer =
 	'			<span>关注微信公众号</span> ' +
 	'		</div> ' +
 	'		<div class="nav-contact-box"> ' +
-	'			<div class="nav-contact-item"> ' +
+	'			<div @click="gotoContact(1)" title="跳转博主GitHub主页" class="nav-contact-item"> ' +
 	'				<img src="img/github.png"> ' +
 	'			</div> ' +
-	'			<div class="nav-contact-item"> ' +
+	'			<div @click="gotoContact(2)" title="跳转博主CSDN主页" class="nav-contact-item"> ' +
 	'				<img src="img/csdn.png"> ' +
 	'			</div> ' +
-	'			<div class="nav-contact-item"> ' +
+	'			<div @click="gotoContact(3)" class="nav-contact-item"> ' +
 	'				<img src="img/wechat.png"> ' +
 	'			</div> ' +
-	'			<div class="nav-contact-item"> ' +
+	'			<div @click="gotoContact(4)" class="nav-contact-item"> ' +
 	'				<img src="img/QQ.png"> ' +
 	'			</div> ' +
-	'			<div class="nav-contact-item"> ' +
+	'			<div @click="gotoContact(5)" title="Email" id="me-email" class="nav-contact-item"> ' +
 	'				<img src="img/email.png"> ' +
 	'			</div> ' +
 	'		</div> ' +
@@ -42,3 +68,31 @@ var footer =
 	'		©2020-浮晓悠羡&nbsp;&nbsp;&nbsp;<span class="icp" @click="gotoICPSystem()">粤ICP备&nbsp;20036399号</span> <img src="img/beian_icon.png"/>公网安备 xxxxxxxxxx号 ' +
 	'	</div> ' +
 	'</div> ';
+
+var wxqrcode = {
+	"title": "微信二维码", 
+	"id": 1, 
+	"start": 0, 
+	"data": [ 
+		{
+			"alt": "加微信申请要备注一下哦",
+			"pid": 1, //图片id
+			"src": "http://static.resource.com/bloger/image/wechat_qrcode.jpg", //原图地址
+			"thumb": "http://static.resource.com/bloger/image/wechat_qrcode_thumb.jpg" //缩略图地址
+		}
+	]
+}
+
+var qqQrcode = {
+	"title": "QQ二维码", 
+	"id": 2, 
+	"start": 0, 
+	"data": [ 
+		{
+			"alt": "加QQ申请要备注一下哦",
+			"pid": 1, //图片id
+			"src": "http://static.resource.com/bloger/image/qq_qrcode.jpg", //原图地址
+			"thumb": "http://static.resource.com/bloger/image/qq_qrcode_thumb.jpg" //缩略图地址
+		}
+	]
+}
